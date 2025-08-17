10fefd2865e510cfeccd746a8a3ee24e066feeef
Azmi Afif <azmiafif0909@gmail.com>
2025-08-17 13:57:20 +0700

    second commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -43,7 +43,7 @@ export default function Home() {
     playsInline
     className="absolute inset-0 w-full h-full object-cover"
   >
-    <source src="/hero.mp4" type="video/mp4" />
+    <source src="/hero1.mp4" type="video/mp4" />
     Your browser does not support the video tag.
   </video>
 
